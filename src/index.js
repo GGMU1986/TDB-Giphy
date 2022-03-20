aaababe9e08dccdc3d49b911c5587573e1102314
ggmu1986 <gt2186@gmail.com>
2022-03-20 12:05:12 -0400

    Set up masonry style layout and fixed no search text positioning

--- src/index.js
+++ src/index.js
@@ -1,40 +1,43 @@
 
-
 document.addEventListener("DOMContentLoaded", () => {
 
   const form = document.getElementById('form');
   const input = document.getElementById('input');
-  const audio = document.createElement('audio')
-  audio.src = './sounds/yeet.mp3';
+  const audioYeet = document.createElement('audio')
+  audioYeet.src = './sounds/yeet.mp3';
+  const audioCmon = document.createElement('audio')
+  audioCmon.src = './sounds/cmon.mp3';
   const APIKEY = '0OLzi5rvV7wVGAsRmX7ugDVPFTjsOKEQ'
   const gifsContainer = document.getElementById('gifs-container');
+  const noSearchValue = document.createElement('div');
+  noSearchValue.classList.add('no-search')
+  noSearchValue.textContent = 'PLEASE ENTER SOMETHING IN SEARCH'
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    gifsContainer.innerHTML = null;
+    gifsContainer.textContent = null;
     let searchValue = form.children[0].value;
     let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=20&q=${searchValue}`
-    audio.play();
-    fetch(url)
-      .then(res => res.json())
-      .then(content => {
-        console.log(content.data)
-        content.data.forEach((gif, i) => {
-          let gifCont = document.createElement('div')
-          gifCont.classList.add('gif-cont')
-          let img = document.createElement('img')
-          // let title = document.createElement('figcaption')
-          // img.style.height = (i % 2 === 1) ? '400px' : '200px';
-          img.src = gif.images.downsized.url;
-          // title.textContent = gif.title;
-          // title.style.textAlign = 'center';
-          gifCont.appendChild(img);
-          // gifCont.appendChild(title);
-          gifsContainer.appendChild(gifCont);
-        });
-      })
-      .catch(err => console.error(err))
-    input.value = '';
+    if (searchValue){
+      audioYeet.play();
+      fetch(url)
+        .then(res => res.json())
+        .then(content => {
+          content.data.forEach((gif, i) => {
+            let gifCont = document.createElement('div')
+            gifCont.classList.add('gif-cont')
+            let img = document.createElement('img')
+            img.src = gif.images.downsized.url;
+            gifCont.appendChild(img);
+            gifsContainer.appendChild(img);
+          });
+        })
+        .catch(err => console.error(err))
+      input.value = '';
+    } else {
+      gifsContainer.append(noSearchValue)
+      audioCmon.play();
+    }
   });
 
 
